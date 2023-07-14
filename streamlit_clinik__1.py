94b0b288f414c6fc054a64aa90d7fb28389097b3
Ramil <122420589+Zeroflip64@users.noreply.github.com>
2023-07-14 15:12:58 +0700

    Update streamlit_clinik__1.py

--- streamlit_clinik__1.py
+++ streamlit_clinik__1.py
@@ -22,7 +22,7 @@ from sklearn.metrics import f1_score,roc_auc_score,precision_score,accuracy_scor
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout,BatchNormalization
 from tensorflow.keras.optimizers import Adam
-
+import joblib
 import streamlit as st
 
 #Model
